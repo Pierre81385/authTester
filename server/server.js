3f2d97c8bf89bbc3b3869ae5831693bbc92ea592
Peter Bishop <pjb.den@gmail.com>
2022-02-24 10:52:15 -0700

    created basic file structure with express server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+// express middleware, used to be bodyparser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
