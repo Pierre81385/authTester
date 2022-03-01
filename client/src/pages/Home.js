e0c3baa268d406d6e26766c0f143c0e26b85239d
Peter Bishop <pjb.den@gmail.com>
2022-03-01 02:07:11 -0700

    comment and reply works

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,12 +15,15 @@ function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [posts, setPosts] = useState([]);
   const [commentsLoaded, setCommentsLoaded] = useState(false);
+  const [replysLoaded, setReplysLoaded] = useState(false);
   const [comments, setComments] = useState([]);
   const [submitted, setSubmitted] = useState(false);
-  const [replySubmitted, setReplySubmitted] = useState(false);
+  const [replysSubmitted, setReplysSubmitted] = useState(false);
+  const [displayComment, setDisplayComment] = useState("none");
+  const [commentButtonDisplay, setCommentButtonDisplay] = useState("inline");
   const [displayReply, setDisplayReply] = useState("none");
-  const [buttonDisplay, setButtonDisplay] = useState("inline")
-  const [replyComment, setReplyComment] = useState("")
+  const [buttonDisplay, setButtonDisplay] = useState("inline");
+  const [replyComment, setReplyComment] = useState("");
   const [commentInfo, setCommentInfo] = useState({
     postCreatedAt: "",
     comment: "",
@@ -30,6 +33,8 @@ function Home() {
     commentCreatedAt: "",
     reply: "",
   });
+  const [replys, setReplys] = useState([]);
+
   const history = useHistory();
 
   //query Firebase Firestore USERS table for user name and image data.  Update name and profileImage state with data.
@@ -79,7 +84,7 @@ function Home() {
         );
         setComments([...data]);
         setCommentsLoaded(true);
-        console.log(comments);
+        console.log("the comments loaded" + comments);
       } catch (error) {
         console.log(error);
       }
@@ -94,6 +99,31 @@ function Home() {
   }, [submitted]);
 
   //query DynamoDB REPLYS table for all replys.  Update replys state with data.  Reupdate if replySubmitted state is true.
+  useEffect(() => {
+    console.log("getting replys");
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/api/replys");
+        const jsonData = await res.json();
+        // sort the array by createdAt property ordered by descending values
+        const data = jsonData.sort((a, b) =>
+          a.createdAt < b.createdAt ? 1 : -1
+        );
+        setReplys([...data]);
+        setReplysLoaded(true);
+        console.log("the replys loaded " + replys);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+
+    if (replysSubmitted) {
+      console.log("new replys submitted");
+      fetchData();
+    }
+  }, [replysSubmitted]);
 
   //on render, check login status and redirect if not logged in, or load user information via fetchUserName()
   useEffect(() => {
@@ -148,22 +178,39 @@ function Home() {
     };
 
     const renderComments = (comment) => {
-
       const showReply = (event) => {
         event.preventDefault();
         setReplyComment(comment.createdAt);
 
         if (displayReply === "none") {
           setDisplayReply("inline");
-          setButtonDisplay("none")
+          setButtonDisplay("none");
         } else {
           setDisplayReply("none");
-          setButtonDisplay("inline")
+          setButtonDisplay("inline");
         }
       };
 
+      const renderReplys = (reply) => {
+        return (
+          <div>
+            {reply.commentCreatedAt === comment.createdAt.toString() ? (
+              <>
+                <h2>{reply.reply}</h2>
+                <h5>
+                  Reply made on {Date(reply.createdAt).toString()} by{" "}
+                  {reply.username}
+                </h5>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        );
+      };
+
       return (
-        <div id="commentDiv" key={comment.createdAt}>
+        <div key={comment.createdAt}>
           {comment.postCreatedAt === post.createdAt.toString() ? (
             <>
               <h2>{comment.comment}</h2>
@@ -171,12 +218,12 @@ function Home() {
                 Comment made on {Date(comment.createdAt).toString()} by{" "}
                 {comment.username}
               </h5>
+              <div>{replys.map(renderReplys)}</div>
               <form onSubmit={showReply} style={style.replyButton}>
                 <button className="btn col-12 " type="submit">
                   Reply
                 </button>
-              </form>
-              {/* elements for replys here */}
+              </form>{" "}
             </>
           ) : (
             <></>
@@ -185,49 +232,60 @@ function Home() {
       );
     };
 
-          //set replyInfo state in preparation for submit
-          const handleReplyChange = (event) => {
-            // console.log(replyComment)
-            if (event.target.value.length <= 280) {
-              setReplyInfo({
-                username: name,
-                commentCreatedAt: replyComment.toString(),
-                reply: event.target.value,
-              });
-              //console.log("firebase user id is: " + commentInfo.firebaseUserId);
-              setReplyCharacterCount(event.target.value.length);
-            }
-          };
-    
-          //submit reply
-          const handleReplyFormSubmit = (event) => {
-            event.preventDefault();
-    
-            const replyData = async () => {
-              const res = await fetch("/api/replys", {
-                method: "POST",
-                headers: {
-                  Accept: "application/json",
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(replyInfo),
-              });
-              const data = await res.json();
-              console.log(data);
-            };
-            replyData();
-    
-            // clear form value
-            setReplyInfo({
-              commentCreatedAt: "",
-              reply: "",
-              //firebaseUserId: "",
-            });
-            setReplyCharacterCount(0);
-            setReplySubmitted(true);
-            setDisplayReply("none")
-            setButtonDisplay("inline")
-          };
+    //set replyInfo state in preparation for submit
+    const handleReplyChange = (event) => {
+      // console.log(replyComment)
+      if (event.target.value.length <= 280) {
+        setReplyInfo({
+          username: name,
+          commentCreatedAt: replyComment.toString(),
+          reply: event.target.value,
+        });
+        //console.log("firebase user id is: " + commentInfo.firebaseUserId);
+        setReplyCharacterCount(event.target.value.length);
+      }
+    };
+
+    //submit reply
+    const handleReplyFormSubmit = (event) => {
+      event.preventDefault();
+
+      const replyData = async () => {
+        const res = await fetch("/api/replys", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(replyInfo),
+        });
+        const data = await res.json();
+        console.log(data);
+      };
+      replyData();
+
+      // clear form value
+      setReplyInfo({
+        commentCreatedAt: "",
+        reply: "",
+        //firebaseUserId: "",
+      });
+      setReplyCharacterCount(0);
+      setReplysSubmitted(true);
+      setDisplayReply("none");
+      setButtonDisplay("inline");
+    };
+    const showComment = (event) => {
+      event.preventDefault();
+
+      if (displayReply === "none") {
+        setDisplayComment("inline");
+        setCommentButtonDisplay("none");
+      } else {
+        setDisplayComment("none");
+        setCommentButtonDisplay("inline");
+      }
+    };
 
     return (
       <div>
@@ -242,7 +300,17 @@ function Home() {
         </Link>
         <h4>{post.description}</h4>
         <div>{comments.map(renderComments)}</div>
-        <form onSubmit={handleFormSubmit}>
+        <form>
+          <button
+            className="btn col-12 "
+            type="submit"
+            onClick={showComment}
+            style={style.commentButton}
+          >
+            Comment
+          </button>
+        </form>
+        <form onSubmit={handleFormSubmit} style={style.showCommentForm}>
           <p className={`m-0 ${characterCount === 280 ? "text-error" : ""}`}>
             Character Count: {characterCount}/280
           </p>
@@ -256,26 +324,46 @@ function Home() {
           <button className="btn col-12 " type="submit">
             Submit
           </button>
+          <button
+            className="btn col-12 "
+            type="submit"
+            style={style.showCommentForm}
+            onClick={() => {
+              setCommentButtonDisplay("inline");
+              setDisplayComment("none");
+            }}
+          >
+            Cancel
+          </button>
         </form>
         <form onSubmit={handleReplyFormSubmit} style={style.replyForm}>
-                <p
-                  className={`m-0 ${
-                    replyCharacterCount === 280 ? "text-error" : ""
-                  }`}
-                >
-                  Character Count: {replyCharacterCount}/280
-                </p>
-                <textarea
-                  placeholder="Reply..."
-                  name="reply"
-                  value={replyInfo.reply}
-                  className="form-input col-12 "
-                  onChange={handleReplyChange}
-                ></textarea>
-                <button className="btn col-12 " type="submit">
-                  Submit
-                </button>
-              </form>
+          <p
+            className={`m-0 ${replyCharacterCount === 280 ? "text-error" : ""}`}
+          >
+            Character Count: {replyCharacterCount}/280
+          </p>
+          <textarea
+            placeholder="Reply..."
+            name="reply"
+            value={replyInfo.reply}
+            className="form-input col-12 "
+            onChange={handleReplyChange}
+          ></textarea>
+          <button className="btn col-12 " type="submit">
+            Submit
+          </button>
+          <button
+            className="btn col-12 "
+            type="submit"
+            style={style.replyForm}
+            onClick={() => {
+              setButtonDisplay("inline");
+              setDisplayReply("none");
+            }}
+          >
+            Cancel
+          </button>
+        </form>
       </div>
     );
   };
@@ -303,8 +391,14 @@ function Home() {
       display: `${displayReply}`,
     },
     replyButton: {
-      display: `${buttonDisplay}`
-    }
+      display: `${buttonDisplay}`,
+    },
+    showCommentForm: {
+      display: `${displayComment}`,
+    },
+    commentButton: {
+      display: `${commentButtonDisplay}`,
+    },
   };
 
   return (
