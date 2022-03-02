512260f8ef0215955bbe542a37c658fa49971bb3
Peter Bishop <pjb.den@gmail.com>
2022-03-02 10:42:12 -0700

    added reactbootstrap dependency and styled login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,20 +1,20 @@
 //Notes:
 //Authorization is handled by Firebase, and currently accepts users created on firebase with email OR a users GMAIL account.
-//Other accounts to be accepted include Facebook, Instagram, and Apple.  
+//Other accounts to be accepted include Facebook, Instagram, and Apple.
 
 import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import {
-  auth,
-  logInWithEmailAndPassword,
-  signInWithGoogle,
-} from "../firebase";
+import { auth, logInWithEmailAndPassword, signInWithGoogle } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Container, Form, Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, loading, error] = useAuthState(auth);
   const history = useHistory();
+
   useEffect(() => {
     if (loading) {
       // maybe trigger a loading screen
@@ -22,43 +22,74 @@ function Login() {
     }
     if (user) history.replace("/");
   }, [user, loading]);
-  return (
-    <div>
-      <div>
-        <form>
-          <input
-            type="text"
-            className="login__textBox"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="E-mail Address"
-          />
-          <input
-            type="password"
-            className="login__textBox"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-          />
-        </form>
 
-        <button
-          className="login__btn"
+  const style = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+    },
+    card: {
+      width: `350px`,
+      marginRight: "auto",
+      marginLeft: "auto",
+      padding: "10px",
+      borderRadius: "2%",
+    },
+    button: {
+      marginTop: "5px",
+      marginBottom: "2px",
+    },
+  };
+
+  return (
+    <Container style={style.container}>
+      <Card style={style.card}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              className="login__textBox"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="E-mail Address"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+
+            <Form.Control
+              type="password"
+              className="login__textBox"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+          </Form.Group>
+        </Form>
+
+        <Button
+          variant="dark"
+          style={style.button}
           onClick={() => logInWithEmailAndPassword(email, password)}
         >
           Login
-        </button>
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
+        </Button>
+        <Button variant="dark" style={style.button} onClick={signInWithGoogle}>
           Login with Google
-        </button>
+        </Button>
         <div>
           <Link to="/reset">Forgot Password</Link>
         </div>
         <div>
           Don't have an account? <Link to="/register">Register</Link> now.
         </div>
-      </div>
-    </div>
+      </Card>
+    </Container>
   );
 }
 export default Login;
